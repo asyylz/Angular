059cc13bfab541b5b48cbf2b95f986c626774741
asyylz <asiyeyaliz@gmail.com>
2025-06-26 18:25:55 +0100

    Implement code changes to enhance functionality and improve performance

--- prj-4- Components and Templates/src/app/dashboard/server-status/server-status.component.ts
+++ prj-4- Components and Templates/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core'
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +7,16 @@ import { Component,OnInit } from '@angular/core'
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit{
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline'
-
+  // private interval?: ReturnType<typeof setInterval>
   constructor() {}
 
+  private destroyRef = inject(DestroyRef)
   ngOnInit() {
-    setInterval(() => {
+    console.log('On init')
+
+    const interval = setInterval(() => {
       const rnd = Math.random() // 0 - 0.9999999999999
 
       if (rnd < 0.5) {
@@ -24,5 +27,15 @@ export class ServerStatusComponent implements OnInit{
         this.currentStatus = 'unknown'
       }
     }, 5000)
+    // Modern way available on newer after 16
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval)
+    })
   }
+  ngAfterViewInit() {
+    console.log('After view init')
+  }
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval)
+  // }
 }
--- prj-4- Components and Templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ prj-4- Components and Templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
-import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component'
+import { ControlComponent } from '../../../shared/control/control.component'
+import { FormsModule } from '@angular/forms'
 // import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
-    selector: 'app-new-ticket',
-    standalone: true,
-    templateUrl: './new-ticket.component.html',
-    styleUrl: './new-ticket.component.css',
-    imports: [ButtonComponent, ControlComponent]
+  selector: 'app-new-ticket',
+  standalone: true,
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+  imports: [ButtonComponent, ControlComponent, FormsModule],
 })
 export class NewTicketComponent {
-
+  onSubmit(title: string, ticketText: string) {
+    console.log(title)
+    console.log(ticketText)
+  }
 }
