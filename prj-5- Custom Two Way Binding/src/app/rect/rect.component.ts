5108b00e8dab63a0c77c0e94fcc8354a689325dd
asyylz <asiyeyaliz@gmail.com>
2025-06-27 10:18:09 +0100

    feat: Implement required model for rectangle dimensions and reset functionality

--- prj-5- Custom Two Way Binding/src/app/rect/rect.component.ts
+++ prj-5- Custom Two Way Binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,12 +8,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-@Input({required:true}) size!: {width:string, height:string}
-@Output() sizeChange = new EventEmitter<{ width:string, height:string}>() // name mustbe same with input to make angular understand it is not up to our choice
+// @Input({required:true}) size!: {width:string, height:string}
+// @Output() sizeChange = new EventEmitter<{ width:string, height:string}>() // name mustbe same with input to make angular understand it is not up to our choice
+size = model.required<{width:string, height:string}>()
   onReset() {
-    this.sizeChange.emit({
-      width:'200',
+    // this.sizeChange.emit({
+    //   width:'200',
+    //   height:'200'
+    // })
+
+  this.size.set({
+       width:'200',
       height:'200'
-    })
-  }
+  })
+}
 }
